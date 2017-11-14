09753ef1d85097c70192ec5d4d1aabdf1932ac93
paddymcconn <paddymcconn1280@hotmail.com>
2017-11-14 15:32:13 +0000

    write to database update

--- WriteQueriesToDB_edit.java
+++ WriteQueriesToDB_edit.java
@@ -1,24 +1,24 @@
-import java.io.File;	
+import java.io.File;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 import java.util.Scanner;
 
 public class WriteQueriesToDB_edit {
 
 	// for sqllite:
-	private static String connectionUrl = "F:\\CLEF2014_data\\Database\\CLEFlab.db"; // 2014
-	// add the url for the database
+	private static String connectionUrl = "C:/Users/u180384/sqlite/EClef.db";
+	// point to the right database
 	// Declare the JDBC objects.
 	private Connection con;
 	private Statement stmt;
 	private ResultSet rs;
 
-	public WriteQueriesToDB_edit(String queriesTableName) { // called queries in the other class
+	public WriteQueriesToDB_edit(String queriesTableName) { // called queries in
+															// the other class
 
 		// Declare the JDBC objects.
 		con = null;
@@ -31,8 +31,8 @@ public class WriteQueriesToDB_edit {
 			con = DriverManager.getConnection("jdbc:sqlite:" + connectionUrl);
 
 			// sqllite server:
-			String sql = "CREATE TABLE if not exists " + queriesTableName
-					+ "(queryNum STRING, queryTerms STRING, description STRING, narr STRING, profile STRING, reportID STRING) ";
+			String sql = "CREATE TABLE if not exists " + queriesTableName + "(queryNum STRING, queryTerms STRING) ";
+			// what we want in the table, test numbers and the translations
 
 			Statement stat = con.createStatement();
 			stat.execute(sql);
@@ -59,7 +59,6 @@ public class WriteQueriesToDB_edit {
 	}
 
 	// writing queries to db
-	// for writing the 2014 queries
 	public void write2014(String pathtofiles, String tableName) {
 
 		// Declare the JDBC objects.
@@ -78,54 +77,30 @@ public class WriteQueriesToDB_edit {
 			FileReader reader = new FileReader(child);
 			Scanner in = new Scanner(reader);
 
-			String line = "";
+			// String line = "";
 
-			line = in.nextLine();// xml first line
-			line = in.nextLine();// <queries> tag
+			// line = in.nextLine();// xml first line
+			// line = in.nextLine();// <queries> tag
 
-			int counter = 0;
-			while (in.hasNextLine() && counter < 5) {
+			while (in.hasNextLine()) {
+				// line = in.nextLine(); // <query> tag
+				String Num = in.nextLine().replace("<num>", "");
+				Num = Num.replace("</num>", "");
 
-				counter++;
-				System.out.println(counter);
+				String Query = in.nextLine().replace("<query>", "");
+				Query = Query.replace("</query>", "");
 
-				line = in.nextLine(); // <query> tag
-
-				String id = in.nextLine().replace("<id>", "");
-				id = id.replace("</id>", "");
-				id = id.replace("QTRAIN", "");
-
-				String reportID = in.nextLine().replace("<discharge_summary>", "");
-				reportID = reportID.replace("</discharge_summary>", "");
-
-				String title = in.nextLine().replace("<title>", "");
-				title = title.replace("</title>", "");
-
-				String desc = in.nextLine().replace("<desc>", "");
-				desc = desc.replace("</desc>", "");
-
-				String profile = in.nextLine().replace("<profile>", "");
-				profile = profile.replace("</profile>", "");
-
-				String narr = in.nextLine().replace("<narr>", "");
-				narr = narr.replace("</narr>", "");
-
-				line = in.nextLine(); // </query> tag
+				// line = in.nextLine(); // </query> tag
 				// line = in.nextLine(); //blank line
 
 				// write details to DB:
-				String SQL1 = "INSERT INTO " + tableName
-						+ " (queryNum, queryTerms, description, narr, profile, reportID) VALUES(?, ?, ?, ?, ?, ?);";
+				String SQL1 = "INSERT INTO " + tableName + " (queryNum, queryTerms) VALUES(?, ?);";
 
 				PreparedStatement pstmt1 = con.prepareStatement(SQL1); // create
 																		// a
 																		// statement
-				pstmt1.setString(1, id);
-				pstmt1.setString(2, title);
-				pstmt1.setString(3, desc);
-				pstmt1.setString(4, narr);
-				pstmt1.setString(5, profile);
-				pstmt1.setString(6, reportID);
+				pstmt1.setString(1, Num);
+				pstmt1.setString(2, Query);
 
 				pstmt1.executeUpdate(); // execute insert statement
 				pstmt1.close();
--- Write_To_Database.java
+++ Write_To_Database.java
@@ -1,14 +1,12 @@
-import java.util.*;
-
 public class Write_To_Database {
 
 	public static void main(String[] args) {
 
-		// write the 5 queries [2014] to db table:
-
-		WriteQueriesToDB_edit wqtdb = new WriteQueriesToDB_edit("Queries");
-		wqtdb.write2014("F:\\CLEF2014_data\\Queries\\queries.clef2014ehealth.1-5.train.en_forDistribution.xml",
-				"Queries");
+		// write the 5 queries [2014] to db table
+		String Table = "Arabic_Queries";
+		WriteQueriesToDB_edit wqtdb = new WriteQueriesToDB_edit(Table);
+		wqtdb.write2014("C:/Users/u180384/Dropbox/Final Year/Final Year Project/test_xml/arabic_With_Tags.txt", Table);
+		// write the given file to the table we call Queries.
 
 	}
 
