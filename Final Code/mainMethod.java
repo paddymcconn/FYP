39cdc23de6c1e06a205d2b3bc4858baaa6c9d63c
paddymcconn <paddymcconn1280@hotmail.com>
2018-03-29 14:11:33 +0100

    Update mainMethod.java

--- Final Code/mainMethod.java
+++ Final Code/mainMethod.java
@@ -4,16 +4,12 @@
  * 
  * NOTE: it is necessary to set paths to lucene, db, file , etc in the
  * individual methods in this code.
- * 
- *
  */
 
 public class mainMethod {
-
 	public static void main(String[] args) {
-
-		// do IR for the queries against the index using Lucene's implementation
-		// of vector space model.
+		// uncomment the ones you want to use.
+		
 		// testRetrieve_DirlechtSmoothing Ds = new
 		// testRetrieve_DirlechtSmoothing();
 		testRetrieve_vsm v = new testRetrieve_vsm();
@@ -23,11 +19,9 @@ public class mainMethod {
 		// Ds.getQs();
 		// create result list in the format required for use with trec eval
 		System.out.println("");
-		// System.out.println("BM_25");
 		CreateTrecEvalResultList cc = new CreateTrecEvalResultList();
 		cc.create("VSMresultsIR_German");
 
 		System.out.println("<<<<<<<<<<<<<FINISHED>>>>>>>>>>>>");
 	}
-
 }
